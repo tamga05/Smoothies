5675975b2fc302bca7946a6d3c6089cad014e8a6
Andrey Nikitin <tamga05@gmail.com>
2021-07-26 18:22:41 +0300

    addition

---
+++ dist/assets/js/script.js
@@ -0,0 +1,9 @@
+function test(a, b) {
+    return a + b
+}
+
+console.log(test(5, 2));
+console.log(test(2, 4));
+console.log(test(9, 1));
+
+//# sourceMappingURL=script.js.map
---
+++ src/assets/js/main.js
@@ -0,0 +1,3 @@
+console.log(test(5, 2));
+console.log(test(2, 4));
+console.log(test(9, 1));
